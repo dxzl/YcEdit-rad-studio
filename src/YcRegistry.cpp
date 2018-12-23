339f042a0bcd92d698d7ec678a5c96f180dbaad8
Scott Swift <dxzl@live.com>
2018-12-23 14:29:15 -0600

    Modify heading comments

--- src/YcRegistry.cpp
+++ src/YcRegistry.cpp
@@ -1,3 +1,7 @@
+// I believe we can safely construe that this software may be released under
+// the Free Software Foundation's GPL - I adopted TaeRichEdit for my project
+// more than 15 years ago and have made significant changes in various
+// places. Below is the original license. (Scott Swift 2015 dxzl@live.com)
 //===========================================================================
 // Copyright © 1999 Thin Air Enterprises and Robert Dunn.  All rights reserved.
 // Free for non-commercial use.  Commercial use requires license agreement.
